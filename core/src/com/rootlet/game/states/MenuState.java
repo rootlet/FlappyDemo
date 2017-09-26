2105ac237025e4b7362bdbd63129a5d025c0d002
Sergey Pavlenko <rootlet@rootlet.com>
2017-09-27 00:32:03 +0300

    Lesson 1

---
+++ core/src/com/rootlet/game/states/MenuState.java
@@ -0,0 +1,46 @@
+package com.rootlet.game.states;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.rootlet.game.FlappyDemo;
+
+/**
+ * Created by pavlenko on 27.09.17.
+ */
+
+public class MenuState extends State{
+
+    private Texture background;
+    private Texture playBtn;
+
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+        background = new Texture("bg.png");
+        playBtn = new Texture("playbtn.png");
+    }
+
+    @Override
+    protected void handlerInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(background, 0, 0, FlappyDemo.WIDTH, FlappyDemo.HEIGTH);
+        sb.draw(playBtn, (FlappyDemo.WIDTH / 2) - (playBtn.getWidth() / 2), FlappyDemo.HEIGTH / 2);
+        sb.end();
+    }
+
+    @Override
+    public void dispose() {
+        background.dispose();
+        playBtn.dispose();
+    }
+}
